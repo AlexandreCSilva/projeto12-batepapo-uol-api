203994a8f836a0762428aa09840f83e074c9708c
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-09-02 16:41:12 -0300

    making the post participants works

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const server = express();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 const Joi = joi;
+const userSchema = joi.object({
+    name: joi.string().required(),
+});
 
 server.use(cors());
 server.use(express.json());
@@ -19,13 +22,32 @@ mongoClient.connect().then(() => {
 });
 
 server.post('/participants', async (req, res) => {
-    const user = req.body;
-    const users = await db.collection('users').find().toArray();
+    /* const user = req.body;
+    const validUser = await db.collection('users').find().toArray();
 
-    if (user){
-        console.log(user)
+    console.log(validUser) */
+    try {
+        const user = req.body;
+        const validUser = await db.collection('users').find(user).toArray();
+
+        console.log(validUser)
+       
+        if (validUser.length !== 0){
+            res.status(409).send('O nome já está sendo usado');
+        } else {
+            const validation = userSchema.validate(user, { abortEarly: true });
+            console.log(validation)
+
+            if (validation.error) {
+                res.status(422).send('Nome inválido');
+            } else {
+                const dbuser = await db.collection('users').insertOne({name: user.name, lastStatus: Date.now()});
+                res.status(201);
+            }
+        }
+    } catch (error) {
+        res.status(500).send('Erro do servidor');
     }
-    
 })
 
 server.listen(5000, () => {console.log('Server on')})
