b5abcde6fb6bf7c851b8bb874fce5119417fefef
AlexandreCSilva <alecosta.silva12@gmail.com>
2022-09-03 00:47:49 -0300

    making the get messages works

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = express();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 const Joi = joi;
 const userSchema = joi.object({
-    name: joi.string().required(),
+    name: joi.string().required()
 });
 
 server.use(cors());
@@ -27,26 +27,35 @@ mongoClient.connect().then(() => {
 
 server.post('/participants', async (req, res) => {
     /* const user = req.body;
-    const validUser = await db.collection('users').find().toArray();
+    const validUser = await db.collection('users').deleteMany({});
 
     console.log(validUser) */
+    
     try {
-        const user = req.body;
+        const user = await req.body;
         const validUser = await db.collection('users').find(user).toArray();
-
+        
         if (validUser.length !== 0){
             res.status(409).send('O nome já está sendo usado');
-        } else {
-            const validation = userSchema.validate(user, { abortEarly: true });
+            return;
+        } 
 
-            if (validation.error) {
-                res.status(422).send('Nome inválido');
-            } else {
-                await db.collection('users').insertOne({name: user.name, lastStatus: Date.now()});
-                await db.collection('messages').insertOne({from: 'xxx', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format("HH:MM:ss")});
-                res.status(201);
-            }
+        const validation = userSchema.validate(user);
+
+        if (validation.error) {
+            res.status(422).send('Nome inválido');
+            return;
         }
+
+        const time =  Date.now();
+        const userSaved = {name: user.name, lastStatus: time};
+        const message = {from: user.name, to: 'Todos', text: 'entra na sala...', type: 'message', time: dayjs(time).format("HH:MM:ss")};
+        
+        await db.collection('users').insertOne(userSaved);
+        await db.collection('messages').insertOne(message);
+        
+        res.status(201);
+        
     } catch (error) {
         res.status(500).send('Erro do servidor');
     }
@@ -57,9 +66,49 @@ server.get('/participants', async (req, res) => {
         const users = await db.collection('users').find().toArray();
         const usersSent = [];
         users.forEach((user) => {
-            usersSent.push({ name: user.name, lastStatus: user.lastStatus});
+            usersSent.push({ name: user.name });
         })
         res.status(201).send(usersSent);
+    } catch (error) {
+        res.status(500).send('Erro do servidor');
+    }
+})
+
+server.get('/messages', async (req, res) => {
+    try {
+        const limit = await req.query.limit;
+        const user = await req.headers.user;
+        const messages = await db.collection('messages').find().toArray();
+        const messagesSent = [];
+
+        if (!limit){
+
+            messages.forEach((message) => {
+                console.log(message.to);
+                if (message.type === 'message' || message.type === 'status'){
+                    messagesSent.push({from: message.from, to: message.to, text: message.text, type: message.type, time: message.time});
+                } else if (message.to === user){
+                    messagesSent.push({from: message.from, to: message.to, text: message.text, type: message.type, time: message.time});
+                }
+            })
+
+            res.status(201).send(messagesSent);
+            return;
+        }
+
+        const dif = messages.length - limit -1;
+
+        for (let i = messages.length ; i >= dif ; i--){
+            if (messages[i]){
+                if (messages[i].type === 'message' || messages[i].type === 'status'){
+                    messagesSent.unshift({from: messages[i].from, to: messages[i].to, text: messages[i].text, type: messages[i].type, time: messages[i].time});
+                } else if (messages[i].to === user){
+                    messagesSent.unshift({from: messages[i].from, to: messages[i].to, text: messages[i].text, type: messages[i].type, time: messages[i].time});
+                }
+            }
+        }
+
+        res.status(201).send(messagesSent);
     } catch (error) {
         res.status(500).send('Erro do servidor');
     }
